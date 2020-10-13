16a78b1033b33948c0e1d633c6ed881524e9b962
HenryJ <henryjung667@gmail.com>
2020-10-13 16:06:41 -0500

    added event rows

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-function clock() {
-    var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
-    $("#currentDay").text(currentTime);
-    console.log(clock)
-}
+$(document).ready(function() {
+    $("#currentDay").text(moment().format("MMM Do YYYY"));
+
+    for (var i = 9; i < 18; i++) {
+
+        //create row
+        var row = $("<div data-time=${i} id='${i}'>");
+        row.addClass("row time-block");
+        //hour
+        var hourDiv = $("<div>");
+        hourDiv.addClass("hour col-1");
+        //text
+        var descriptionText = $("<textarea>");
+        descriptionText.addClass("description col past")
+        descriptionText.attr("placeholder", "add event here")
+        //save button
+        var saveButton = $("<button>");
+        saveButton.addClass("saveBtn col-1 fas fa-save");
+        //append hour text and save button to row
+        row.append(hourDiv);
+        row.append(descriptionText);
+        row.append(saveButton);
+        //append row to container
+        $(".container").append(row);
+    }
+})
