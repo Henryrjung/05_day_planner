1e6fdfd85621df77075c94391f27a80e0a886c16
HenryJ <henryjung667@gmail.com>
2020-10-14 15:09:41 -0500

    reworked jquery

--- script.js
+++ script.js
@@ -1,26 +1,10 @@
-$(document).ready(function() {
-    $("#currentDay").text(moment().format("MMM Do YYYY"));
 
+$(document).ready(function() {
     for (var i = 9; i < 18; i++) {
 
-        //create row
-        var row = $("<div data-time=${i} id='${i}'>");
-        row.addClass("row time-block");
-        //hour
-        var hourDiv = $("<div>");
-        hourDiv.addClass("hour col-1");
-        //text
-        var descriptionText = $("<textarea>");
-        descriptionText.addClass("description col past")
-        descriptionText.attr("placeholder", "add event here")
-        //save button
-        var saveButton = $("<button>");
-        saveButton.addClass("saveBtn col-1 fas fa-save");
-        //append hour text and save button to row
-        row.append(hourDiv);
-        row.append(descriptionText);
-        row.append(saveButton);
-        //append row to container
-        $(".container").append(row);
+       var row = $("<div>").addClass("row time-block").appendTo(".container");
+        $("<div>").addClass("hour col-1").appendTo(row);
+        $("<textarea>").addClass("description col past").attr("placeholder", "add event here").appendTo(row);
+        $("<button>").addClass("saveBtn col-1 fas fa-save").appendTo(row);
     }
 })
